eaee049b2b9cb05d021bb9483a07294a65b8435f
benchang02 <changb3@rpi.edu>
2016-03-25 16:41:19 -0400

    3/25/2016
    Motion Reworked
    Began work on JSON Export to Player Prefs

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -12,10 +12,10 @@ public class Move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-/*		if (tracker.objs.ContainsKey("Wand")) {
+		if (tracker.objs.ContainsKey("Wand")) {
 			Tracker_Obj wand = tracker.objs["Wand"];
-			transform.position = new Vector3(1000 * wand.x, 1000 * wand.y, 1000 * wand.z);
-			transform.rotation = Quaternion.Euler(wand.h, wand.p, wand.r);*/
-		//}
+			transform.position = new Vector3(10 * wand.x, 10 * wand.y, 10 * wand.z);
+			transform.rotation = Quaternion.Euler(wand.h, wand.p, wand.r);
+		}
 	}
 }
--- Assets/Tracking/MotionControl.cs
+++ Assets/Tracking/MotionControl.cs
@@ -3,19 +3,60 @@ using UnityEditor;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using SimpleJSON;
 
+public class Orientation {
+	public Vector3 pos;
+	public Quaternion rot;
+	public Orientation() {
+		pos = new Vector3();
+		rot = Quaternion.identity;
+	}
+	public Orientation(Vector3 _pos, Quaternion _rot) {
+		pos = _pos;
+		rot = _rot;
+	}
+	public Orientation(Transform transform) {
+		pos = transform.position;
+		rot = transform.rotation;
+	}
+}
 	
+public class MotionControl : MonoBehaviour {
+
+	private int frame;
+	private bool loop;
+	private List<Orientation> motion;
+	private Orientation starting;
+
+	public IEnumerator Move(List<Orientation> _motion, bool _loop = false) {
+		frame = 0;
+		loop = _loop;
+		motion = _motion;
+		starting = new Orientation(gameObject.transform);
+
+		while (motion != null) {
+			gameObject.transform.position = motion[frame].pos;
+			gameObject.transform.rotation = motion[frame].rot;
+
+			frame += 1 % motion.Count;
+			if (frame == 0 && !loop)
+				motion = null;
+			yield return null;
+		}
+	}
+}
 
 
 // Editor
 [CustomEditor(typeof(MotionControl))]
 public class MotionEditor : Editor {
 
-	public Dictionary<String, Motion> motions;
+	public Dictionary<String, List<Orientation>> motions;
 	public MotionControl motionControl; 
 
 	void OnEnable() {
-		motions = new Dictionary<String, Motion>();
+		motions = new Dictionary<String, List<Orientation>>();
 		motionControl = (MotionControl)target;
 	}
 
@@ -23,11 +64,11 @@ public class MotionEditor : Editor {
 
 		DrawDefaultInspector ();
 
-		foreach (KeyValuePair <string, Motion> entry in motions) {
+		foreach (KeyValuePair <string, List<Orientation>> entry in motions) {
 			EditorGUILayout.BeginHorizontal();
 			GUILayout.Label(entry.Key);
 			if (GUILayout.Button("Play", GUILayout.Width(50))) {
-				motionControl.animation = entry.Value;
+				motionControl.Move(entry.Value);
 			}
 			EditorGUILayout.EndHorizontal();
 		}
@@ -45,7 +86,6 @@ public class MotionBuilder : EditorWindow {
 	
 	MotionEditor editor;
 	List<Orientation> record; 
-	Motion recorded_motion;
 	string name;
 	bool recording;
 	Transform startingTrans;
@@ -56,17 +96,22 @@ public class MotionBuilder : EditorWindow {
 	}
 
 	void Update() {
+
+		if (!EditorApplication.isPlaying) {
+			Debug.Log("running");
+			Close();
+		}
+
 		// Record the frames of motion
 		if (recording) {
 			Orientation frame = new Orientation();
-			frame.pos = (editor.motionControl.startingPos - editor.motionControl.gameObject.transform.position);
-			frame.rot = editor.motionControl.gameObject.transform.rotation;
+			frame.pos = startingTrans.position - editor.motionControl.gameObject.transform.position;
+			frame.rot = Quaternion.FromToRotation(startingTrans.forward, editor.motionControl.gameObject.transform.forward);
 			record.Add(frame);
 		}
 	}
 
 	void OnGUI() {
-
 		GUI.Label(new Rect(5, 4, 80, 18), "Enter a name:");
 
 		// Assign name from text field
@@ -76,14 +121,16 @@ public class MotionBuilder : EditorWindow {
 			// Begin Recording
 			if (GUI.Button(new Rect(5, 28, 50, 18), "Record")) {
 				record = new List<Orientation>();
-				editor.motionControl.startingPos = editor.motionControl.gameObject.transform.position;
+				startingTrans = editor.motionControl.gameObject.transform;
 				recording = true;
 			}
 			// Allow Submission of a Recording
-			if (!recording && record.Count > 0) {
+			if (!recording && record != null && record.Count > 0) {
 				if (GUI.Button(new Rect(60, 28, 50, 18), "Submit")) {
-					editor.motions.Add(name, recorded_motion);
-					editor.motionControl.animation = null;
+					// http://answers.unity3d.com/questions/753058/get-a-json-string-from-a-simplejson-node.html
+					<< syntax error >>
+					JSONNode motion = new JSONNode();
+					editor.motions.Add(name, record);
 					this.Close();
 				}
 			}
@@ -92,13 +139,11 @@ public class MotionBuilder : EditorWindow {
 			// End Recording
 			if (GUI.Button(new Rect(5, 28, 50, 18), "Stop")) {
 				recording = false;
-				recorded_motion = new Motion(record);
-				recorded_motion.FixedAnimate(editor.motionControl);
+				editor.motionControl.Move(record);
 			}
 		}
 		// Cancel Motion Creation
 		if (GUI.Button(new Rect(170, 28, 50, 18), "Cancel")) {
-			editor.motionControl.animation = null;
 			this.Close();
 		}
 	}
